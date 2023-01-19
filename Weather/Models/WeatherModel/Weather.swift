8db3583d4a1471a80a1b74e9dee05323d68c1bb0
Nikita Moshyn <m.nikitozzz@gmail.com>
2023-01-19 20:32:36 +0200

    Final V1

--- Weather/Models/WeatherModel/Weather.swift
+++ Weather/Models/WeatherModel/Weather.swift
@@ -13,10 +13,4 @@ struct Weather: Codable {
     let location: Location?
     let current: Current?
     let forecast: Forecast?
-
-//    enum CodingKeys: String, CodingKey {
-//        case location = "location"
-//        case current = "current"
-//        case forecast = "forecast"
-//    }
 }
